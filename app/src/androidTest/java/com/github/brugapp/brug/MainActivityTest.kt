9903d3ae280a1f32ac96f3cce9952e67e112897b
habib <hassan.habib@epfl.ch>
2022-03-07 22:39:13 +0100

    Some dependencies added
    Some dependencies about the qr code scanner added to make sure that
    everything work

--- app/src/androidTest/java/com/github/brugapp/brug/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/MainActivityTest.kt
@@ -1,10 +1,11 @@
 package com.github.brugapp.brug
 
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,7 +22,22 @@ class MainActivityTest {
     @Test
     fun textViewDisplaysCorrectText() {
         // Context of the app under test.
-        Espresso.onView(ViewMatchers.withId(R.id.mainHelloWorld))
-            .check(ViewAssertions.matches(ViewMatchers.withText("Welcome to Unlost!")))
+        onView(withId(R.id.mainHelloWorld))
+            .check(ViewAssertions.matches(withText("Welcome to Unlost!")))
     }
+}
+@RunWith(AndroidJUnit4::class)
+class QrCodeScannerActivityTest {
+    @get:Rule
+    var qrCodeScannerActivityRule = ActivityScenarioRule(QrCodeScannerActivity::class.java)
+
+    //https://stackoverflow.com/questions/33929937/android-marshmallow-test-permissions-with-espresso
+    @get:Rule var permissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.CAMERA)
+
+    @Test
+    fun hintTextIsCorrect(){
+        onView(withHint("Report item…"))
+            .check(ViewAssertions.matches(isDisplayed()))
+    }
+
 }
--- app/src/main/java/com/github/brugapp/brug/MainActivity.kt
+++ app/src/main/java/com/github/brugapp/brug/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.github.brugapp.brug
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendReportLostItem(view: View){
+        val intent = Intent(this,QrCodeScannerActivity::class.java).apply{}
+        startActivity(intent)
     }
 }
