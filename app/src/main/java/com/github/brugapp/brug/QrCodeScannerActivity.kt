34252f89ecabb38d1bdc3f8fdcda8a246f5c7b30
habib <hassan.habib@epfl.ch>
2022-03-07 22:15:17 +0100

    Added a new activity that allows us to scan qr Code
    New Activity added that allow the user to report an object by scanning
    the qr code or to enter the ID of the object
    A test was added to for this feature

---
+++ app/src/main/java/com/github/brugapp/brug/QrCodeScannerActivity.kt
@@ -0,0 +1,91 @@
+package com.github.brugapp.brug
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+
+private const val CAMERA_REQUEST_CODE = 101
+
+//library found on github: https://github.com/yuriy-budiyev/code-scanner
+class QrCodeScannerActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_scanner)
+        setupPermissions()
+        codeScanner()
+    }
+
+    private fun codeScanner() {
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        codeScanner = CodeScanner(this, scannerView)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    findViewById<EditText>(R.id.editTextReportItem).setText(it.text)
+                }
+            }
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("", "Camera initialization error: ${it.message}")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                CAMERA_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You need the camera permission", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+
+}
