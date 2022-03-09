acb49bc1c7eaf21853fdc030dd1e3eccebdc3019
DeepGreen1 <jean.etienne.charbonnet@protonmail.com>
2022-03-09 18:41:22 +0100

    Creation of a UI for the Settings Activity

---
+++ app/src/main/java/com/github/brugapp/brug/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.github.brugapp.brug
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
