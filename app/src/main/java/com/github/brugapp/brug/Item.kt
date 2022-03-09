cc8ab9fbebceb8d373e6121462d4441495b0c1d9
Hamza Hassoune <hamza@tsf-476-wpa-2-212.epfl.ch>
2022-03-09 15:50:05 +0100

    Item class structure

---
+++ app/src/main/java/com/github/brugapp/brug/Item.kt
@@ -0,0 +1,43 @@
+package com.github.brugapp.brug
+
+import java.lang.IllegalArgumentException
+
+class Item {
+
+    private var name : String
+    private var id : Int
+    // add QR code attribute
+    // add last localization attribute
+
+    // Generate the id later
+    constructor(name : String, id : Int){
+
+        if(name.isNullOrBlank()){
+            throw IllegalArgumentException("Invalid name")
+        }
+
+        this.name = name
+        this.id = id
+
+    }
+
+    fun getName() : String{
+        return name
+    }
+
+    fun getId() : Int{
+        return id
+    }
+
+    fun setName(name : String){
+
+        if(name.isNullOrBlank()){
+            throw IllegalArgumentException("Invalid new name")
+        }
+
+        this.name = name
+
+    }
+
+
+}
---
+++ app/src/test/java/com/github/brugapp/brug/ItemTest.kt
@@ -0,0 +1,83 @@
+package com.github.brugapp.brug
+
+import org.junit.Test
+import org.junit.Assert.*
+import java.lang.IllegalArgumentException
+
+class ItemTest {
+
+    @Test
+    fun invalidNameItem(){
+        var blankName = "    "
+        assertThrows(IllegalArgumentException::class.java){
+            var invalidUser = Item(blankName, 1)
+        }
+
+    }
+
+    @Test
+    fun emptyNameItem(){
+        var emptyName = ""
+        assertThrows(IllegalArgumentException::class.java){
+            var invalidUser = Item(emptyName, 1)
+        }
+    }
+
+    @Test
+    fun validNameItem(){
+        var itemName = "Wallet"
+        var itemId = 1
+        var validUser = Item(itemName, itemId)
+
+        assert(validUser.getName().equals(itemName))
+
+
+    }
+
+    @Test
+    fun validIdItem(){
+        var itemName = "Wallet"
+        var itemId = 1
+        var validUser = Item(itemName, itemId)
+
+        assert(validUser.getId() == itemId)
+
+    }
+
+    @Test
+    fun setInvalidName(){
+        var itemName = "Wallet"
+        var itemId = 1
+        var validUser = Item(itemName, itemId)
+
+        var invalidName = "    "
+
+        assertThrows(IllegalArgumentException::class.java){
+            validUser.setName(invalidName)
+        }
+    }
+
+    @Test
+    fun setValidName(){
+        var itemName = "Wallet"
+        var itemId = 1
+        var validUser = Item(itemName, itemId)
+
+        var validName = "Bag"
+
+
+        validUser.setName(validName)
+
+        assert(validUser.getName().equals(validName))
+    }
+
+
+
+
+}
+
+
+
+
+
+
