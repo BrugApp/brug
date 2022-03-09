1970559cd5b559a7866f428718f046c812c4ae0f
habib <hassan.habib@epfl.ch>
2022-03-09 10:45:48 +0100

    grant permission for the camera
    We do not need to ask everytime for using the camera

--- app/src/main/java/com/github/brugapp/brug/QrCodeScannerActivity.kt
+++ app/src/main/java/com/github/brugapp/brug/QrCodeScannerActivity.kt
@@ -1,15 +1,9 @@
 package com.github.brugapp.brug
 
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.*
 
 private const val CAMERA_REQUEST_CODE = 101
@@ -23,11 +17,8 @@ class QrCodeScannerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_code_scanner)
-        setupPermissions()
         codeScanner()
     }
-
-
 
     private fun codeScanner() {
         val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
@@ -61,37 +52,5 @@ class QrCodeScannerActivity : AppCompatActivity() {
         codeScanner.releaseResources()
         super.onPause()
     }
-
-    private fun setupPermissions() {
-        val permission = ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.CAMERA
-        )
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.CAMERA),
-                CAMERA_REQUEST_CODE
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            CAMERA_REQUEST_CODE -> {
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "You need the camera permission", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-        }
-    }
-
-
 
 }
