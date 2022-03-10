0159e7c2f9c6d292bf04aeee93e794b980186f11
DeepGreen1 <45233492+DeepGreen1@users.noreply.github.com>
2022-03-10 17:54:22 +0100

    Feature/settings activity (#52)
    * Creation of a UI for the Settings Activity
    
    * Add tests for the settings page

--- app/src/main/java/com/github/brugapp/brug/MainActivity.kt
+++ app/src/main/java/com/github/brugapp/brug/MainActivity.kt
@@ -2,6 +2,8 @@ package com.github.brugapp.brug
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -22,12 +24,21 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.launchbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.action_settings -> {
+            startActivity(Intent(this, SettingsActivity::class.java))
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
 
     fun sendReportLostItem(view: View){
         val intent = Intent(this,QrCodeScannerActivity::class.java).apply{}
         startActivity(intent)
     }
 }
-
-
-
