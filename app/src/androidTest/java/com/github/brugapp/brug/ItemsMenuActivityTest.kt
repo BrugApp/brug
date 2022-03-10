5439fb04a1bbdc52bd7067c65150f44ab4201454
Mounir Raki <mouniraki@protonmail.com>
2022-03-10 10:45:46 +0100

    Add tests for ItemsMenuActivity

--- app/src/androidTest/java/com/github/brugapp/brug/ItemsMenuActivityTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/ItemsMenuActivityTest.kt
@@ -3,26 +3,113 @@ package com.github.brugapp.brug
 import android.app.Activity
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+//import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.intent.matcher.BundleMatchers.hasEntry
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.core.IsEqual
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.IOException
+import java.lang.RuntimeException
+
+private const val DUMMY_TEXT = "Actual behavior coming soon..."
 
 @RunWith(AndroidJUnit4::class)
 class ItemsMenuActivityTest {
+    @get:Rule
+    val testRule = ActivityScenarioRule(ItemsMenuActivity::class.java)
+
     @Test
-    fun dummyItemsTest(){
-        Intents.init()
+    fun changingBottomNavBarMenuToItemsListChangesFocus(){
+        val itemsListMenuButton = onView(withId(R.id.items_list_menu_button))
+        itemsListMenuButton.perform(click()).check(matches(isEnabled()))
+    }
 
-        ActivityScenario.launch(ItemsMenuActivity::class.java).use {
+    @Test
+    fun changingBottomNavBarMenuToQRScanChangesFocus(){
+        val scanQRMenuButton = onView(withId(R.id.qr_scan_menu_button))
+        scanQRMenuButton.perform(click()).check(matches(isEnabled()))
+    }
 
+    @Test
+    fun changingBottomNavBarMenuToChatChangesFocus(){
+        val chatMenuButton = onView(withId(R.id.chat_menu_button))
+        chatMenuButton.perform(click()).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun clickingOnSettingsButtonTriggersSnackBar(){
+        val settingsButton = onView(withId(R.id.my_settings))
+        val snackBar = onView(withId(com.google.android.material.R.id.snackbar_text))
+        settingsButton.perform(click())
+        snackBar.check(matches(withText(DUMMY_TEXT)))
+    }
+
+    /* THESE NEED A CONFLICTING ESPRESSO DEPENDENCY */
+    @Test
+    fun swipeLeftOnItemDeletesItem(){
+        val itemsList = onView(withId(R.id.items_listview))
+//        itemsList.perform(
+//            RecyclerViewActions.scrollTo<ItemsCustomAdapter.ViewHolder>(
+//                hasDescendant(withText("not in the list"))
+//            )
+//        )
+
+    }
+
+    @Test
+    fun swipeRightOnItemDeletesItem(){
+        val itemsList = onView(withId(R.id.items_listview))
+
+    }
+
+    @Test
+    fun dragUpOnItemReordersList(){
+        val itemsList = onView(withId(R.id.items_listview))
+
+    }
+
+    @Test
+    fun dragDownOnItemReordersList(){
+        val itemsList = onView(withId(R.id.items_listview))
+
+    }
+
+
+    @Test
+    fun checkifKeyboardIsShownAfterPressingSearch() {
+        val searchButton = onView(withId(R.id.search_items))
+        searchButton.perform(click())
+        assertThat(isKeyboardOpenedShellCheck(), IsEqual(true))
+    }
+
+
+
+
+    // Companion functions
+    private fun isKeyboardOpenedShellCheck(): Boolean {
+        val checkKeyboardCmd = "dumpsys input_method | grep mInputShown"
+
+        try {
+            return UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+                .executeShellCommand(checkKeyboardCmd).contains("mInputShown=true")
+        } catch (e: IOException) {
+            throw RuntimeException("Keyboard check failed", e)
         }
-
-        Intents.release()
-
     }
 }
