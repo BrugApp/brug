c32ead07d662c1a81773cfa0b15cb8782628f586
Omar El Malki <56803362+omarelmalki@users.noreply.github.com>
2022-03-11 01:13:11 +0100

    Integrate User-Item model with Sign In (#55)
    Fixed unclosed activity scenario in a few tests.
    Integrated User class with SignIn Account result to check for compatibility and catch problems early.

--- app/src/main/java/com/github/brugapp/brug/model/User.kt
+++ app/src/main/java/com/github/brugapp/brug/model/User.kt
@@ -1,6 +1,5 @@
 package com.github.brugapp.brug.model
 
-import java.lang.IllegalArgumentException
 import java.util.regex.Pattern
 
 class User// later generate id
@@ -8,13 +7,12 @@ class User// later generate id
     private var firstName: String,
     private var lastName: String,
     private var email: String,
-    private var id: Int
+    private var id: String
 ) {
-
-    private var items : ArrayList<Item>
+    private var items: ArrayList<Item>
 
     init {
-        if(firstName.isBlank() || lastName.isBlank()){
+        if (firstName.isBlank() || lastName.isBlank() || id.isBlank()) {
             throw IllegalArgumentException("Invalid name !")
         }
         val EMAIL_ADDRESS_PATTERN = Pattern.compile(
@@ -26,7 +24,7 @@ class User// later generate id
                     "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
                     ")+"
         )
-        if(!EMAIL_ADDRESS_PATTERN.matcher(email).matches()){
+        if (!EMAIL_ADDRESS_PATTERN.matcher(email).matches()) {
 
             throw IllegalArgumentException("Invalid email !")
 
@@ -34,22 +32,21 @@ class User// later generate id
         items = ArrayList()
     }
 
-    fun getFirstName() : String {
+    fun getFirstName(): String {
         return firstName
     }
 
-    fun getLasttName() : String {
+    fun getLastName(): String {
         return lastName
     }
 
-    fun getId() : Int {
+    fun getId(): String {
         return id
     }
 
-    fun getEmail() : String {
+    fun getEmail(): String {
         return email
     }
-
 
 
 }
