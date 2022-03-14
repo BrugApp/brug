056eca52f13fd050ef47c735f98799e02ce69d35
Mounir Raki <mouniraki@protonmail.com>
2022-03-14 14:58:34 +0100

    Add Chat Menu UI building blocks

--- app/src/main/java/com/github/brugapp/brug/ChatMenuActivity.kt
+++ app/src/main/java/com/github/brugapp/brug/ChatMenuActivity.kt
@@ -2,10 +2,24 @@ package com.github.brugapp.brug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+
+private const val SEARCH_HINT = "Search for a conversation…"
 
 class ChatMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_menu)
+    }
+
+    // Initializing the top-bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_top_bar, menu)
+
+        val conversation = menu?.findItem(R.id.search_box)
+        val searchView = conversation?.actionView as androidx.appcompat.widget.SearchView
+
+        searchView.queryHint = SEARCH_HINT
+        return super.onCreateOptionsMenu(menu)
     }
 }
