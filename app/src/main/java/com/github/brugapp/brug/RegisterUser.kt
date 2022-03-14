5f0f4c0cf0692490675ae878d927e4b21b0f7355
Paul Luc Mc Intyre <paul.mcintyre@epfl.ch>
2022-03-14 09:42:39 +0100

    correct user fields in database
    correct register activity for functional name/itemlist fields

--- app/src/main/java/com/github/brugapp/brug/RegisterUser.kt
+++ app/src/main/java/com/github/brugapp/brug/RegisterUser.kt
@@ -7,14 +7,18 @@ import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
 class RegisterUser : AppCompatActivity(), View.OnClickListener{
 
+    private val db = Firebase.firestore
+    private var progressBar: ProgressBar? = null
     private var mAuth: FirebaseAuth? = null
     private var firstName: EditText? = null
     private var lastName: EditText? = null
@@ -29,6 +33,7 @@ class RegisterUser : AppCompatActivity(), View.OnClickListener{
         var regUser = findViewById<Button>(R.id.registerbutton)
         regUser.setOnClickListener(this)
 
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
         firstName = findViewById<EditText>(R.id.firstname)
         lastName = findViewById<EditText>(R.id.lastName)
         email = findViewById<EditText>(R.id.emailAddressReg)
@@ -38,10 +43,10 @@ class RegisterUser : AppCompatActivity(), View.OnClickListener{
 
     override fun onClick(v: View?) {
         //we clicked the register button
-        var firstnametxt = email?.getText().toString().trim()
-        var lastNametxt = email?.getText().toString().trim()
+        var firstnametxt = firstName?.getText().toString().trim()
+        var lastNametxt = lastName?.getText().toString().trim()
         var emailtxt = email?.getText().toString().trim()
-        var passwordtxt = email?.getText().toString().trim()
+        var passwordtxt = password?.getText().toString().trim()
 
         if(firstnametxt.isEmpty()){
             firstName?.setError("Please enter first name")
@@ -88,14 +93,33 @@ class RegisterUser : AppCompatActivity(), View.OnClickListener{
             }
         }
         */
+        progressBar?.setVisibility(View.VISIBLE)
         mAuth?.createUserWithEmailAndPassword(emailtxt, passwordtxt)
             ?.addOnCompleteListener(
                 this
             ) { task ->
                 if (task.isSuccessful) {
+
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
-                    val user = mAuth!!.currentUser
+                    val user = mAuth?.currentUser
+                    val list = listOf<String>()
+                    val userToAdd = hashMapOf(
+                        "ItemIDArray" to list,
+                        "UserID" to (user?.uid ?: String),
+                        "email" to emailtxt,
+                        "firstName" to firstnametxt,
+                        "lastName" to lastNametxt
+                    )
+                    db.collection("Users").add(userToAdd)
+                        .addOnSuccessListener { documentReference ->
+                            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w(TAG, "Error adding document", e)
+                        }
+                    progressBar?.setVisibility(View.GONE)
+                    //db.collection("Users").document("")
                     //updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
@@ -104,6 +128,7 @@ class RegisterUser : AppCompatActivity(), View.OnClickListener{
                         this@RegisterUser, "Authentication failed.",
                         Toast.LENGTH_SHORT
                     ).show()
+                    progressBar?.setVisibility(View.GONE)
                     //updateUI(null)
                 }
 
