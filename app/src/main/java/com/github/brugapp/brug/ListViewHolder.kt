50559751821bc0b383674cf0171b33d64e510b82
Mounir Raki <57262470+Mouniraki@users.noreply.github.com>
2022-03-17 19:07:09 +0100

    Add UI for Chat Menu Activity (#78)
    * Merge branch 'main' of github.com:BrugApp/brug
    
    * Refactored initNavigationBar() function in ItemsMenuActivity
    
    Now handling transitions between activities using a proper handler
    
    * Add Chat Menu UI building blocks
    
    * Add pseudo-final implementation of Chat Menu UI
    
    * Refactor code to avoid confusions with new Chat Menu UI implementation
    
    * Add test class for Chat Menu UI
    
    * Refactored code, with new class handling list callbacks
    
    * Removed unnecessary parent closures in list item layouts
    
    * Refactored code to avoid duplications in Chat Activity & Items Activity
    
    * More refactoring of code related to lists
    
    * Add tests for ChatMenuActivity + missing tests for drag
    
    * Add tests for ChatMenuActivity + missing tests for drag
    
    * Rename ListCallbackImpl class to ListCustomCallback
    
    * Add new CustomTopBar class to avoid code duplications
    
    * Add new inflateTopBar function in CustomTopBar to avoid more code duplications
    
    * Rewrite swipe tests to use UIAutomator
    
    * Code cleanup in test classes according to PR comments

---
+++ app/src/main/java/com/github/brugapp/brug/ListViewHolder.kt
@@ -0,0 +1,24 @@
+package com.github.brugapp.brug
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListViewHolder (view: View,
+                      iconId: Int,
+                      titleId: Int,
+                      descId: Int,
+                      onItemClicked: (Int) -> Unit
+) : RecyclerView.ViewHolder(view){
+
+        init{
+            itemView.setOnClickListener {
+                onItemClicked(adapterPosition)
+            }
+        }
+
+        val icon: ImageView = view.findViewById(iconId)
+        val title: TextView = view.findViewById(titleId)
+        val desc: TextView = view.findViewById(descId)
+}
