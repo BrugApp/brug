cdc919267988ba3335fe7fd1c6ceba8940f130cd
Omar El Malki <homerdon0@gmail.com>
2022-03-24 20:23:49 +0100

    Remove useless placeholder text and add tests.

--- app/src/androidTest/java/com/github/brugapp/brug/MyFCMMessagingServiceTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/MyFCMMessagingServiceTest.kt
@@ -1,11 +1,19 @@
 package com.github.brugapp.brug
 
+import android.os.Bundle
+import com.github.brugapp.brug.messaging.MyFCMMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import org.junit.Test
 
 class MyFCMMessagingServiceTest {
 
     @Test
-    fun onTokenDoesNothingYetTest() {
+    fun onNewTokenDoesNothingYetTest() {
+        MyFCMMessagingService().onNewToken("")
+    }
 
+    @Test
+    fun onMessageReceivedDoesNotCrashIfNotificationIsNullTest() {
+        MyFCMMessagingService().onMessageReceived(RemoteMessage(Bundle.EMPTY))
     }
 }
