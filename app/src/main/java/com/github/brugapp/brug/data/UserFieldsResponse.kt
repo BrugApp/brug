7600c9aa8a3a3e66b77fd600409dc2ed6a4b6b77
Mounir Raki <mouniraki@protonmail.com>
2022-04-05 00:14:01 +0200

    Update version of kotlin-coroutines import

--- app/src/main/java/com/github/brugapp/brug/data/UserFieldsResponse.kt
+++ app/src/main/java/com/github/brugapp/brug/data/UserFieldsResponse.kt
@@ -5,4 +5,4 @@ import java.io.Serializable
 /**
  * Holds either a pair (fullname, iconPath), or an exception.
  */
-data class ConvUserResponse(var onSuccess: Pair<String, String>? = null, var onError: Exception? = null): Serializable
+data class UserFieldsResponse(var onSuccess: Pair<String, FileResponse>? = null, var onError: Exception? = null): Serializable
