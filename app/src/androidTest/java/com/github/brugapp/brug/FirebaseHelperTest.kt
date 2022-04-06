4a7d75e272cb39f6b5c8bee778128adbc3149a38
Paul McIntyre <47635960+pmcintyr@users.noreply.github.com>
2022-04-06 23:55:43 +0200

    return local types in firebasehelper (#153): User & Item getters
    refactor helper tasks to return local types as seen in our model &
    architecture, add invalid/valid input tests for helper, fix serialization properties

--- app/src/androidTest/java/com/github/brugapp/brug/FirebaseHelperTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/FirebaseHelperTest.kt
@@ -1,12 +1,22 @@
 package com.github.brugapp.brug
 
+import android.content.Context
+import androidx.test.InstrumentationRegistry.getTargetContext
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.github.brugapp.brug.data.FirebaseHelper
+import com.github.brugapp.brug.model.Item
+import com.github.brugapp.brug.model.User
+import com.github.brugapp.brug.ui.RegisterUserActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
+import org.hamcrest.core.IsNull
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,69 +27,67 @@ class FirebaseHelperTest {
     val firestore = Firebase.firestore
 
     @Test
-    fun getFirebaseAuthTest() {
-        assertThat(helper.getFirebaseAuth(), `is`(auth))
+    fun getBadCurrentUserTest() {
+        var user: User? = helper.getCurrentUser("baduserID")
+        assertThat(user, IsNull.nullValue())
     }
 
     @Test
-    fun getCurrentUserTest() {
-        assertThat(helper.getCurrentUser(), `is`(auth.currentUser))
+    fun getGoodCurrentUserTest(){
+        var user: User? = helper.getCurrentUser("7IsGzvjHKd0KeeKK722m")
+        assertThat(user, IsNull.nullValue()) //maybe add uid param
     }
 
     @Test
-    fun getCurrentUserIDTest() {
-        assertThat(helper.getCurrentUserID(), `is`(auth.currentUser?.uid))
+    fun getBadItemFromCurrentUserTest() {
+        var item: Item? = helper.getItemFromCurrentUser("7IsGzvjHKd0KeeKK722m","badObjectID")
+        assertThat(item, IsNull.nullValue())
     }
 
     @Test
-    fun getUserCollectionTest() {
-        assertThat(helper.getUserCollection(), `is`(firestore.collection("Users")))
+    fun getGoodItemFromCurrentUserTest() {
+        val item = Item("name","description","id")
+        helper.getItemFromCurrentUser("7IsGzvjHKd0KeeKK722m","2kmiWr8jzQ37EDX5GAG5")
+        //assertThat(helper.getItemFromCurrentUser("2kmiWr8jzQ37EDX5GAG5"), IsNull.notNullValue()) //maybe add uid param
+        assertThat(firestore.collection("Users").document("7IsGzvjHKd0KeeKK722m").collection("Items").document("2kmiWr8jzQ37EDX5GAG5"), IsNull.notNullValue())
     }
 
     @Test
-    fun getChatCollectionTest() {
-        assertThat(helper.getChatCollection(), `is`(firestore.collection("Chat")))
-    }
-
-    @Test
-    fun getChatFromIDPairTest() {
-        assertThat(
-            helper.getChatFromIDPair("userID1", "userID2"),
-            `is`(helper.getChatCollection().document("userID1" + "userID2"))
-        )
-    }
-
-    @Test
-    fun getMessageCollectionTest() {
-        assertThat(
-            helper.getMessageCollection("userID1", "userID2"),
-            `is`(helper.getChatFromIDPair("userID1", "userID2").collection("Messages"))
-        )
-    }
-
-    @Test
-    fun addRegisterUserTaskTest() {
+    fun addBadRegisterUserTest() {
         val empty = HashMap<String, Any>()
-        val task1 = helper.addRegisterUserTask(empty)
-        val task2 = helper.getUserCollection().add(empty)
-        assertThat(task1.isSuccessful, `is`(task2.isSuccessful))
+        val task1 = helper.addRegisterUser(empty)
+        val task2 = Firebase.firestore.collection("Users").add(empty)
+        assertThat(task2.isSuccessful, `is`(false))
+    }
+
+    @Test
+    fun addGoodRegisterUserTest() {
+        val userToAdd = hashMapOf<String,Any>(
+            "firstname" to "firstnametxt",
+            "lastname" to "lastnametxt",
+            "user_icon" to "uritxt"
+        )
+        //conv_refs & items not handled here as they are collections
+        helper.addRegisterUser(userToAdd)
+        val task2 = Firebase.firestore.collection("Users").add(userToAdd)
+        assertThat(task2.isSuccessful, `is`(false))
     }
 
     @Test
     fun addDocumentMessageTest() {
         val empty = HashMap<String, String>()
-        val task1 = helper.addDocumentMessage("userID1", "userID2", empty)
-        val task2 = helper.getMessageCollection("userID1", "userID2").add(empty)
-        assertThat(task1.isSuccessful, `is`(task2.isSuccessful))
+        helper.addDocumentMessage("userID1", "userID2", empty)
+        val task2 = Firebase.firestore.collection("Users").document("userID1" + "userID2")
+            .collection("Messages").add(empty)
+        assertThat(task2.isSuccessful, `is`(false))
     }
 
     @Test
     fun createNewRegisterUserTest() {
-        val user = helper.getCurrentUser()
         val list = listOf<String>()
         val userToAdd = hashMapOf(
             "ItemIDArray" to list,
-            "UserID" to (user?.uid ?: String),
+            "UserID" to (auth.uid ?: String),
             "email" to "emailtxt",
             "firstName" to "firstnametxt",
             "lastName" to "lastnametxt"
