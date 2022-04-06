d74ec3d1e238c3998e9c000960a26ad744edcf66
DeepGreen1 <jean.etienne.charbonnet@protonmail.com>
2022-04-06 21:07:11 +0200

    Add chat image from gallery feature Add a new UI for the Chat activity

--- app/src/main/java/com/github/brugapp/brug/view_model/ChatMenuViewModel.kt
+++ app/src/main/java/com/github/brugapp/brug/view_model/ChatMenuViewModel.kt
@@ -3,7 +3,10 @@ package com.github.brugapp.brug.view_model
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import com.github.brugapp.brug.fake.MockDatabase
-import com.github.brugapp.brug.model.*
+import com.github.brugapp.brug.model.ChatMessage
+import com.github.brugapp.brug.model.Conversation
+import com.github.brugapp.brug.model.Item
+import com.github.brugapp.brug.model.User
 import java.time.LocalDateTime
 
 
@@ -22,37 +25,63 @@ class ChatMenuViewModel : ViewModel() {
         println("===========================================================")
         return mutableListOf(
             Conversation(
-                User("Anna", "Rosenberg", "anna@rosenberg.com", "123456",null),
+                User("Anna", "Rosenberg", "anna@rosenberg.com", "123456", null),
                 Item("AirPods", "My beloved AirPods", "0"),
-                mutableListOf(ChatMessage(
-                    "Me",
-                    0,
-                    LocalDateTime.now(),
-                    "Where are you located ? I'm near the center of Lausanne, so feel free to propose me any location in Lausanne"))),
-            Conversation(
-                User("Henry", "Crawford", "crawform@services.co.uk", "129271", MockDatabase.currentUser.getProfilePicture() ),
-                Item("Wallet", "With all my belongings", "0"),
-                mutableListOf(ChatMessage(
-                    "Me",
-                    0,
-                    LocalDateTime.now(),
-                    "Hey ! I might have found your wallet yesterday near the EPFL campus"))),
-            Conversation(
-                User("Jenna", "Hewitt", "jenna.hewitt@epfl.ch", "310827",null),
-                Item("Keys", "Home keys", "0"),
-                mutableListOf(ChatMessage(
-                    "Me",
-                    0,
-                    LocalDateTime.now(),
-                    "Fine, lets meet on Saturday then !"))),
-            Conversation(
-                User("John", "Newmann", "john@microsoft.com", "1892122",null),
-                Item("Smartphone", "Galaxy S22", "0"),
-                mutableListOf(ChatMessage(
+                mutableListOf(
+                    ChatMessage(
                         "Me",
                         0,
                         LocalDateTime.now(),
-                        "Give me my money back you thief !!!")
+                        "Where are you located ? I'm near the center of Lausanne, so feel free to propose me any location in Lausanne"
+                    )
+                )
+            ),
+            Conversation(
+                User(
+                    "Henry",
+                    "Crawford",
+                    "crawform@services.co.uk",
+                    "129271",
+                    MockDatabase.currentUser.getProfilePicture()
+                ),
+                Item("Wallet", "With all my belongings", "0"),
+                mutableListOf(
+                    ChatMessage(
+                        "Henry",
+                        0,
+                        LocalDateTime.now(),
+                        "Hey ! I might have found your wallet yesterday near the EPFL campus"
+                    ),
+                    ChatMessage(
+                        "Me",
+                        0,
+                        LocalDateTime.now(),
+                        "Give me it back!!"
+                    )
+                )
+            ),
+            Conversation(
+                User("Jenna", "Hewitt", "jenna.hewitt@epfl.ch", "310827", null),
+                Item("Keys", "Home keys", "0"),
+                mutableListOf(
+                    ChatMessage(
+                        "Me",
+                        0,
+                        LocalDateTime.now(),
+                        "Fine, lets meet on Saturday then !"
+                    )
+                )
+            ),
+            Conversation(
+                User("John", "Newmann", "john@microsoft.com", "1892122", null),
+                Item("Smartphone", "Galaxy S22", "0"),
+                mutableListOf(
+                    ChatMessage(
+                        "Me",
+                        0,
+                        LocalDateTime.now(),
+                        "Give me my money back you thief !!!"
+                    )
                 )
             )
         )
