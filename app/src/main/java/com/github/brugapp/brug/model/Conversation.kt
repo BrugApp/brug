f5d147a537e3953e2e16a45383db7e3941a92661
Mounir Raki <mouniraki@protonmail.com>
2022-04-17 15:24:04 +0200

    Refactor Conversation to hold a MyUser instead of User (temporary)

--- app/src/main/java/com/github/brugapp/brug/model/Conversation.kt
+++ app/src/main/java/com/github/brugapp/brug/model/Conversation.kt
@@ -5,7 +5,7 @@ import java.io.Serializable
 
 data class Conversation(
     val convId: String,
-    val userFields: DummyUser,
+    val userFields: MyUser,
     val lostItemName: String,
     val messages: MutableList<Message>
 ): Serializable
