ebe088bc078cb99424229f2e654beb7809358c56
Mounir Raki <mouniraki@protonmail.com>
2022-04-17 15:22:29 +0200

    Add new tests for the new repo classes

--- app/src/androidTest/java/com/github/brugapp/brug/ConvRepoTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/ConvRepoTest.kt
@@ -1,4 +1,62 @@
 package com.github.brugapp.brug
 
+import android.util.Log
+import com.github.brugapp.brug.data.ConvRepo
+import com.github.brugapp.brug.data.UserRepo
+import com.github.brugapp.brug.model.MyUser
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual
+import org.hamcrest.core.IsNot
+import org.hamcrest.core.IsNull
+import org.junit.Before
+import org.junit.Test
+
+private val USER1 = MyUser("USER1", "Rayan", "Kikou", null)
+private val USER2 = MyUser("USER2", "Hamza", "Hassoune", null)
+private const val DUMMY_ITEM_NAME = "Airpods"
+
 class ConvRepoTest {
+    //NEEDED SINCE @Before FUNCTIONS NEED TO BE VOID
+    private fun addTestUsers() = runBlocking{
+        UserRepo.addAuthUser(USER1)
+        UserRepo.addAuthUser(USER2)
+    }
+
+    @Before
+    fun setUp() {
+        addTestUsers()
+    }
+
+
+    @Test
+    fun addNewConvCorrectlyReturns() = runBlocking {
+        assertThat(ConvRepo.addNewConversation(USER1.uid, USER2.uid, DUMMY_ITEM_NAME).onSuccess, IsEqual(true))
+    }
+
+    @Test
+    fun getWrongUserConvReturnsNull() = runBlocking {
+        assertThat(ConvRepo.getUserConvFromUID("WRONGCONVID"), IsNull.nullValue())
+    }
+
+    @Test
+    fun getValidConvCorrectlyReturns() = runBlocking {
+        assertThat(ConvRepo.getUserConvFromUID(USER1.uid), IsNot(IsNull.nullValue()))
+    }
+
+    @Test
+    fun deleteNonexistentConvReturnsError() = runBlocking {
+        assertThat(ConvRepo.deleteConversationFromID("WRONGCONVID", USER1.uid).onError, IsNot(IsNull.nullValue()))
+    }
+
+    @Test
+    fun deleteConvNotBelongingToUserReturnsError() = runBlocking {
+        assertThat(ConvRepo.deleteConversationFromID("${USER1.uid}${USER2.uid}", "WRONGUID").onError, IsNot(IsNull.nullValue()))
+    }
+
+    @Test
+    fun deleteValidConvReturnsSuccessfully() = runBlocking {
+        ConvRepo.addNewConversation(USER1.uid, USER2.uid, DUMMY_ITEM_NAME)
+        assertThat(ConvRepo.deleteConversationFromID("${USER1.uid}${USER2.uid}", USER1.uid).onSuccess, IsEqual(true))
+    }
 }
--- app/src/androidTest/java/com/github/brugapp/brug/ItemRepoTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/ItemRepoTest.kt
@@ -1,4 +1,62 @@
 package com.github.brugapp.brug
 
-class MessageRepoTest {
+import com.github.brugapp.brug.data.ItemsRepo
+import com.github.brugapp.brug.data.UserRepo
+import com.github.brugapp.brug.model.MyItem
+import com.github.brugapp.brug.model.MyUser
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual
+import org.hamcrest.core.IsNot
+import org.hamcrest.core.IsNull
+import org.junit.Before
+import org.junit.Test
+
+private val USER = MyUser("USER1", "Rayan", "Kikou", null)
+private var ITEM = MyItem("DUMMYID", "AirPods Pro Max", 0, "My Beloved AirPods", false)
+
+class ItemRepoTest {
+    //NEEDED SINCE @Before FUNCTIONS NEED TO BE VOID
+    private fun addUser() = runBlocking {
+        UserRepo.addAuthUser(USER)
+    }
+
+    @Before
+    fun setUp() {
+        addUser()
+    }
+
+    @Test
+    fun addItemToWrongUserReturnsError() = runBlocking {
+        assertThat(ItemsRepo.addItemToUser(ITEM, "WRONGUID").onError, IsNot(IsNull.nullValue()))
+    }
+
+    @Test
+    fun addItemReturnsSuccessfully() = runBlocking {
+        assertThat(ItemsRepo.addItemToUser(ITEM, USER.uid).onSuccess, IsEqual(true))
+    }
+
+    @Test
+    fun updateNonExistentItemReturnsError() = runBlocking {
+        val updatedItem = MyItem("WRONGITEMID", "AirPods 3", 1, ITEM.getItemDesc(), ITEM.isLost())
+        assertThat(ItemsRepo.updateItemFields(updatedItem, USER.uid).onError, IsNot(IsNull.nullValue()))
+    }
+
+    @Test
+    fun updateItemReturnsSuccessfully() = runBlocking {
+        ItemsRepo.addItemToUser(ITEM, USER.uid)
+        val updatedItem = MyItem(ITEM.itemID, "AirPods 3", 1, ITEM.getItemDesc(), ITEM.isLost())
+        assertThat(ItemsRepo.updateItemFields(updatedItem, USER.uid).onSuccess, IsEqual(true))
+    }
+
+    @Test
+    fun deleteWrongItemReturnsError() = runBlocking {
+        assertThat(ItemsRepo.deleteItemFromUser("WRONGITEMID", USER.uid).onError, IsNot(IsNull.nullValue()))
+    }
+
+    @Test
+    fun deleteItemReturnsSuccessfully() = runBlocking {
+        assertThat(ItemsRepo.deleteItemFromUser(ITEM.itemID, USER.uid).onSuccess, IsEqual(true))
+    }
+
 }
