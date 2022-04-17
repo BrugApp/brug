64316b0b1dc4fa20c051d07682e36d9ad3d3920a
Mounir Raki <mouniraki@protonmail.com>
2022-04-17 15:23:01 +0200

    Add temp classes with modifications for User and Item objects

--- app/src/main/java/com/github/brugapp/brug/model/MyItem.kt
+++ app/src/main/java/com/github/brugapp/brug/model/MyItem.kt
@@ -1,4 +1,66 @@
 package com.github.brugapp.brug.model
 
-class MyItem {
+import com.github.brugapp.brug.R
+import java.io.Serializable
+
+class MyItem(val itemID: String,
+             private var itemName: String,
+             private var itemTypeID: Int,
+             private var itemDesc: String,
+             private var isLost: Boolean): Serializable {
+
+    /* ITEM NAME */
+    fun getItemName(): String {
+        return this.itemName
+    }
+
+    fun setItemName(newName: String) {
+        this.itemName = newName
+    }
+
+
+    /* ITEM TYPE */
+    fun getRelatedItemType(): ItemType {
+        return if(0 <= itemTypeID && itemTypeID < ItemType.values().size)
+            ItemType.values()[itemTypeID]
+        else ItemType.Other
+    }
+
+    fun getItemTypeID(): Int {
+        return this.itemTypeID
+    }
+
+    fun setItemTypeID(typeID: Int) {
+        this.itemTypeID = typeID
+    }
+
+    fun getRelatedIcon(): Int {
+        return when(getRelatedItemType()){
+            ItemType.Wallet -> R.drawable.ic_baseline_account_balance_wallet_24
+            ItemType.Keys -> R.drawable.ic_baseline_vpn_key_24
+            ItemType.CarKeys -> R.drawable.ic_baseline_car_rental_24
+            ItemType.Phone -> R.drawable.ic_baseline_smartphone_24
+            ItemType.Other -> R.drawable.ic_baseline_add_24
+        }
+    }
+
+    /* ITEM DESC */
+    fun getItemDesc(): String {
+        return this.itemDesc
+    }
+
+    fun setItemDesc(newDesc: String) {
+        this.itemDesc = newDesc
+    }
+
+
+    /* ITEM STATE */
+    fun isLost(): Boolean{
+        return this.isLost
+    }
+
+    fun changeLostStatus() {
+        this.isLost = !this.isLost
+    }
+
 }
--- app/src/main/java/com/github/brugapp/brug/model/MyUser.kt
+++ app/src/main/java/com/github/brugapp/brug/model/MyUser.kt
@@ -1,4 +1,62 @@
 package com.github.brugapp.brug.model
 
-class MyUser {
+import android.graphics.drawable.Drawable
+import java.io.Serializable
+
+class MyUser(
+    val uid: String,
+    val firstName: String,
+    val lastName: String,
+    private var userIcon: Drawable?): Serializable {
+
+    // DECLARED HERE TO AVOID HAVING TO INITIALIZE THE LISTS FOR USERS IN CONVERSATIONS
+    private lateinit var itemsList: MutableList<MyItem>
+    private lateinit var convList: MutableList<Conversation>
+
+    /* MINIMALUSER FIELDS */
+    fun getFullName(): String {
+        return "$firstName $lastName"
+    }
+
+    fun getUserIcon(): Drawable? {
+        return this.userIcon
+    }
+
+    fun setUserIcon(newIcon: Drawable?) {
+        this.userIcon = newIcon
+    }
+
+
+    /* ITEMS LIST */
+    fun initItemsList(list: List<MyItem>?){
+        this.itemsList = mutableListOf()
+        if (list != null) {
+            this.itemsList.addAll(list)
+        }
+    }
+
+    fun addItemToList(item: MyItem): Boolean{
+        return this.itemsList.add(item)
+    }
+
+    fun deleteItemAtPos(position: Int): MyItem {
+        return this.itemsList.removeAt(position)
+    }
+
+
+    /* CONVERSATION LIST */
+    fun initConvList(list: List<Conversation>?){
+        this.convList = mutableListOf()
+        if(list != null){
+            this.convList.addAll(list)
+        }
+    }
+
+    fun addConvToList(conv: Conversation): Boolean {
+        return this.convList.add(conv)
+    }
+
+    fun deleteConvAtPos(position: Int): Conversation {
+        return this.convList.removeAt(position)
+    }
 }
