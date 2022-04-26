235215db3197883f3f3a4017e286591b5172ab4b
Omar El Malki <homerdon0@gmail.com>
2022-04-26 13:48:07 +0200

    Add display of mapbox map in its activity

--- app/src/main/java/com/github/brugapp/brug/ui/MapBoxActivity.kt
+++ app/src/main/java/com/github/brugapp/brug/ui/MapBoxActivity.kt
@@ -3,18 +3,30 @@ package com.github.brugapp.brug.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.brugapp.brug.R
+import com.github.brugapp.brug.data.mapbox.LocationPermissionHelper
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import java.lang.ref.WeakReference
 
 var mapView: MapView? = null
 
 class MapBoxActivity : AppCompatActivity() {
+
+    private lateinit var locationPermissionHelper: LocationPermissionHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_box)
         mapView = findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+        locationPermissionHelper = LocationPermissionHelper(WeakReference(this))
+        locationPermissionHelper.checkPermissions {
+            onMapReady()
+        }
     }
+
+    private fun onMapReady() {}
 
     override fun onStart() {
         super.onStart()
@@ -34,5 +46,14 @@ class MapBoxActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mapView?.onDestroy()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
