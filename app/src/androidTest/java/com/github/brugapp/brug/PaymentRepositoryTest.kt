d0450e6105170cf599cab0baf59f2d785648a77a
Paul Luc Mc Intyre <paul.mcintyre@epfl.ch>
2022-04-29 09:02:07 +0200

    correct test return types

--- app/src/androidTest/java/com/github/brugapp/brug/PaymentRepositoryTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/PaymentRepositoryTest.kt
@@ -3,6 +3,7 @@ package com.github.brugapp.brug
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.brugapp.brug.data.PaymentRepository
 import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.core.IsNull
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,13 +14,15 @@ class PaymentRepositoryTest {
     @Test
     fun paymentRequestTest(){
         val item = PaymentRepository.paymentRequest()
-        MatcherAssert.assertThat(item, IsNull.notNullValue())
+        val item2 = PaymentRepository.paymentRequest()
+        MatcherAssert.assertThat(item, `is`(item2))
     }
 
     @Test
     fun updateOnSuccessTest(){
         val update = PaymentRepository.updateOnSuccess()
-        MatcherAssert.assertThat(update, IsNull.notNullValue())
+        val update2 = PaymentRepository.updateOnSuccess()
+        MatcherAssert.assertThat(update, `is`(update2))
     }
 
 
