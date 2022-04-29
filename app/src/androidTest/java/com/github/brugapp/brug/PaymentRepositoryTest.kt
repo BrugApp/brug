dd4e35389c5e4eb4f604694319d1f14f55eb1a16
Paul Luc Mc Intyre <paul.mcintyre@epfl.ch>
2022-04-29 09:29:12 +0200

    correct payment tests

--- app/src/androidTest/java/com/github/brugapp/brug/PaymentRepositoryTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/PaymentRepositoryTest.kt
@@ -3,7 +3,6 @@ package com.github.brugapp.brug
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.brugapp.brug.data.PaymentRepository
 import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers.`is`
 import org.hamcrest.core.IsNull
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,15 +13,13 @@ class PaymentRepositoryTest {
     @Test
     fun paymentRequestTest(){
         val item = PaymentRepository.paymentRequest()
-        val item2 = PaymentRepository.paymentRequest()
-        MatcherAssert.assertThat(item, `is`(item2))
+        MatcherAssert.assertThat(item, IsNull.notNullValue())
     }
 
     @Test
     fun updateOnSuccessTest(){
         val update = PaymentRepository.updateOnSuccess()
-        val update2 = PaymentRepository.updateOnSuccess()
-        MatcherAssert.assertThat(update, `is`(update2))
+        MatcherAssert.assertThat(update, IsNull.notNullValue())
     }
 
 
