0202eafb3abeebc071bc5fe853df8db02edf8156
Paul Luc Mc Intyre <paul.mcintyre@epfl.ch>
2022-04-29 02:26:08 +0200

    optimize imports for paymentRepoTest

--- app/src/androidTest/java/com/github/brugapp/brug/PaymentRepositoryTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/PaymentRepositoryTest.kt
@@ -1,11 +1,9 @@
 package com.github.brugapp.brug
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.brugapp.brug.data.PaymentRepository
 import org.hamcrest.MatcherAssert
 import org.hamcrest.core.IsNull
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
