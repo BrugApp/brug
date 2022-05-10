a621e9fe6e6c2e82edeee8dc710479a01ec89bcf
Paul McIntyre <47635960+pmcintyr@users.noreply.github.com>
2022-05-10 21:55:27 +0200

    Change msgs to var

--- app/src/main/java/com/github/brugapp/brug/view_model/NFCScanViewModel.kt
+++ app/src/main/java/com/github/brugapp/brug/view_model/NFCScanViewModel.kt
@@ -52,7 +52,7 @@ class NFCScanViewModel : ViewModel() {
 
     fun rawMessageToMessage(intent: Intent): Pair<Boolean,Array<NdefMessage>> {
         val rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-        val messages: Array<NdefMessage> = arrayOf<NdefMessage>()
+        var messages: Array<NdefMessage> = arrayOf<NdefMessage>()
         var bool: Boolean = false
             if (rawMessages!=null) {
                 messages = Array<NdefMessage>(rawMessages!!.size) { i -> rawMessages[i] as NdefMessage }
