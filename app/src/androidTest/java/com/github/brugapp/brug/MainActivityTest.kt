65d9684823b8b3bdd40e9f7963e36025d6af2f8f
Omar El Malki <homerdon0@gmail.com>
2022-05-12 18:38:47 +0200

    Delete obsolete tests

--- app/src/androidTest/java/com/github/brugapp/brug/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/MainActivityTest.kt
@@ -1,29 +1,22 @@
 package com.github.brugapp.brug
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import com.github.brugapp.brug.ui.NavigationMenuActivity
 import com.github.brugapp.brug.ui.QrCodeScannerActivity
 import com.github.brugapp.brug.ui.SignInActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.allOf
 import org.junit.After
 import org.junit.Before
-
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,7 +31,8 @@ class MainActivityTest {
     @get:Rule
     var mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
 
-    @get:Rule var permissionRule: GrantPermissionRule = GrantPermissionRule
+    @get:Rule
+    var permissionRule: GrantPermissionRule = GrantPermissionRule
         .grant(android.Manifest.permission.CAMERA)
 
     @get:Rule
@@ -76,7 +70,7 @@ class MainActivityTest {
 
 
     @Test
-    fun canSeeHintWhenCameraIsClicked(){
+    fun canSeeHintWhenCameraIsClicked() {
         onView(withId(R.id.mainCamera)).perform(click())
         onView(withId(R.id.editTextReportItem))
             .check(matches((withHint("Report item…"))))
@@ -91,18 +85,6 @@ class MainActivityTest {
             allOf(
                 IntentMatchers.toPackage("com.github.brugapp.brug"),
                 hasComponent(SignInActivity::class.java.name)
-            )
-        )
-    }
-
-    @Test
-    fun clickingOnMapButtonGoesToCorrectActivity() {
-        Espresso.onView(ViewMatchers.withId(R.id.mapButton)).perform(ViewActions.click())
-
-        Intents.intended(
-            Matchers.allOf(
-                IntentMatchers.toPackage("com.github.brugapp.brug"),
-                IntentMatchers.hasComponent(NavigationMenuActivity::class.java.name)
             )
         )
     }
