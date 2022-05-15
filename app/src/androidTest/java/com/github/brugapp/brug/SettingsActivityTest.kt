3b6a10e4262ff6c5f260355e070d1c8a49c30627
Omar El Malki <homerdon0@gmail.com>
2022-05-15 20:21:42 +0200

    Test BrugApp

---
+++ app/src/androidTest/java/com/github/brugapp/brug/SettingsActivityTest.kt
@@ -0,0 +1,30 @@
+package com.github.brugapp.brug
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.brugapp.brug.ui.SettingsActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class SettingsActivityTest {
+    @get:Rule
+    var mainActivityRule = ActivityScenarioRule(SettingsActivity::class.java)
+
+    @get:Rule
+    var rule = HiltAndroidRule(this)
+
+    @Test
+    fun settingsTest() {
+        Espresso.onView(ViewMatchers.withId(R.id.titleSettings))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Settings")))
+    }
+
+}
