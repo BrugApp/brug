686bae13f41b9965ab290a9348e9e5904b67538c
Mounir Raki <mouniraki@protonmail.com>
2022-05-19 19:47:43 +0200

    Add comment to lastLocation function

--- app/src/main/java/com/github/brugapp/brug/data/ItemsRepository.kt
+++ app/src/main/java/com/github/brugapp/brug/data/ItemsRepository.kt
@@ -62,6 +62,15 @@ object ItemsRepository {
         return response
     }
 
+    /**
+     * Sets the last location of an item.
+     *
+     * @param uid the user ID of the owner of the item
+     * @param itemID the ID of the item
+     * @param lastLocation the last location of the item
+     *
+     * @return FirebaseResponse object denoting if the action was successful
+     */
     suspend fun addLastLocation(
         uid: String,
         itemID: String,
