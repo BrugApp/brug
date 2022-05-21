5385a8f6ffcb19aea5d0d6a777dd75da37324f29
Hamza Hassoune <hamza.hassoune@epfl.ch>
2022-05-21 02:04:55 +0200

    Commented QrCodeShowActivityTest class

--- app/src/androidTest/java/com/github/brugapp/brug/QrCodeShowActivityTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/QrCodeShowActivityTest.kt
@@ -1,32 +1,32 @@
-package com.github.brugapp.brug
-
-import android.content.Intent
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.brugapp.brug.model.ItemType
-import com.github.brugapp.brug.model.MyItem
-import com.github.brugapp.brug.model.MyUser
-import com.github.brugapp.brug.ui.QrCodeShowActivity
-import org.junit.Test
-import org.junit.runner.RunWith
-
-@RunWith(AndroidJUnit4::class)
-class QrCodeShowActivityTest{
-    private val item = MyItem("Phone", ItemType.Phone.ordinal,"my phone", false)
-    private val code:String = "0:23"
-    private val user= MyUser("0","Brug", "App",null, mutableListOf())
-
-    private val infoListIntent = Intent(
-        ApplicationProvider.getApplicationContext(),
-        QrCodeShowActivity::class.java
-    ).apply {
-        item.setItemID("23")
-        putExtra("qrId", user.uid+":"+item.getItemID())
-    }
-
-}
+//package com.github.brugapp.brug
+//
+//import android.content.Intent
+//import androidx.test.core.app.ActivityScenario
+//import androidx.test.core.app.ApplicationProvider
+//import androidx.test.espresso.Espresso.onView
+//import androidx.test.espresso.assertion.ViewAssertions.matches
+//import androidx.test.espresso.matcher.ViewMatchers
+//import androidx.test.espresso.matcher.ViewMatchers.withText
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+//import com.github.brugapp.brug.model.ItemType
+//import com.github.brugapp.brug.model.MyItem
+//import com.github.brugapp.brug.model.MyUser
+//import com.github.brugapp.brug.ui.QrCodeShowActivity
+//import org.junit.Test
+//import org.junit.runner.RunWith
+//
+//@RunWith(AndroidJUnit4::class)
+//class QrCodeShowActivityTest{
+//    private val item = MyItem("Phone", ItemType.Phone.ordinal,"my phone", false)
+//    private val code:String = "0:23"
+//    private val user= MyUser("0","Brug", "App",null, mutableListOf())
+//
+//    private val infoListIntent = Intent(
+//        ApplicationProvider.getApplicationContext(),
+//        QrCodeShowActivity::class.java
+//    ).apply {
+//        item.setItemID("23")
+//        putExtra("qrId", user.uid+":"+item.getItemID())
+//    }
+//
+//}
