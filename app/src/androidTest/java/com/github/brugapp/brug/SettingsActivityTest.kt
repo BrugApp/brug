bf5810a6dc6e21c692e784a3e64a60ce20c121bb
DeepGreen1 <jean.etienne.charbonnet@protonmail.com>
2022-05-21 23:59:53 +0200

    Add tests of the main

--- app/src/androidTest/java/com/github/brugapp/brug/SettingsActivityTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/SettingsActivityTest.kt
@@ -1,10 +1,13 @@
 package com.github.brugapp.brug
 
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -34,6 +37,12 @@ class SettingsActivityTest {
         onView(withId(R.id.changeProfilePictureButton)).perform(click())
         intended(hasComponent(ProfilePictureSetActivity::class.java.name))
         Intents.release()
+    }
+
+    @Test
+    fun settingsTest() {
+        Espresso.onView(ViewMatchers.withId(R.id.titleSettings))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Settings")))
     }
 }
 
