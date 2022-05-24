d621b708c6a8ab09a1ad01efa407d42fa8da29d8
habib <hassan.habib@epfl.ch>
2022-05-24 15:08:27 +0200

    Add dummy message

--- app/src/androidTest/java/com/github/brugapp/brug/data/ConvRepositoryTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/data/ConvRepositoryTest.kt
@@ -12,6 +12,7 @@ import com.github.brugapp.brug.di.sign_in.brug_account.BrugSignInAccount
 import com.github.brugapp.brug.fake.FirebaseFakeHelper
 import com.github.brugapp.brug.model.Conversation
 import com.github.brugapp.brug.model.Item
+import com.github.brugapp.brug.model.Message
 import com.github.brugapp.brug.model.User
 import com.github.brugapp.brug.model.message_types.PicMessage
 import com.github.brugapp.brug.model.services.DateService
@@ -30,6 +31,7 @@ import org.junit.Test
 import java.io.File
 import java.io.FileOutputStream
 import java.time.LocalDateTime
+import java.time.Month
 
 
 private const val USER_ID1 = "USER1"
@@ -150,7 +152,13 @@ class ConvRepositoryTest {
             .user
         assertThat(firebaseAuth.currentUser, IsNot(IsNull.nullValue()))
         assertThat(firebaseAuth.currentUser!!.uid, IsEqual(authUser!!.uid))
-        ConvRepository.addNewConversation(USER_ID1, USER_ID2, "$USER_ID1:${DUMMY_ITEM.getItemID()}", null, firestore)
+        val dummyDate = DateService.fromLocalDateTime(
+        LocalDateTime.of(
+            2022, Month.MARCH, 23, 15, 30
+        )
+    )
+        val message = Message("BigBoy",dummyDate,"Wagwan")
+        ConvRepository.addNewConversation(USER_ID1, USER_ID2, "$USER_ID1:${DUMMY_ITEM.getItemID()}", message, firestore)
 
         val file = File.createTempFile("tempIMG", ".jpg")
         val fos = FileOutputStream(file)
