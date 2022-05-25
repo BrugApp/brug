f56ccb360dcf3ce3a9fad6b6428d844c5cde2a88
habib <hassan.habib@epfl.ch>
2022-05-25 23:01:09 +0200

    Deleted fragment for profile picture

--- app/src/androidTest/java/com/github/brugapp/brug/SettingsActivityTest.kt
+++ app/src/androidTest/java/com/github/brugapp/brug/SettingsActivityTest.kt
@@ -1,29 +1,18 @@
 package com.github.brugapp.brug
 
-import android.R.attr.bitmap
 import android.app.Activity
 import android.app.Instrumentation
-import android.content.ContentResolver
-import android.content.Context
 import android.content.Intent
-import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.net.Uri
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import androidx.activity.result.ActivityResultRegistry
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.annotation.DrawableRes
-import androidx.core.app.ActivityOptionsCompat
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
@@ -33,21 +22,14 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.brugapp.brug.data.UserRepository
 import com.github.brugapp.brug.di.sign_in.brug_account.BrugSignInAccount
-import com.github.brugapp.brug.di.sign_in.module.ActivityResultModule
 import com.github.brugapp.brug.fake.FirebaseFakeHelper
 import com.github.brugapp.brug.model.User
 import com.github.brugapp.brug.ui.SettingsActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 import org.hamcrest.Description
@@ -62,7 +44,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.internal.matchers.Null
 import java.io.File
 import java.io.FileOutputStream
 import java.util.*
@@ -74,38 +55,9 @@ private const val TEST_USERNAME = "Rayan Kikou"
  * Settings Activity Tests
  */
 @HiltAndroidTest
-@UninstallModules(ActivityResultModule::class)
 @RunWith(AndroidJUnit4::class)
 class SettingsActivityTest {
 
-    @Module
-    @InstallIn(ActivityComponent::class)
-    object FakeImageModule {
-        @Provides
-        fun provideFakeActivityRegistry(@ActivityContext activity: Context): ActivityResultRegistry {
-            val resources: Resources = activity.resources
-            val resId = R.mipmap.ic_launcher
-            val expectedResult = Uri.parse(
-                ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + resources.getResourcePackageName(
-                    resId
-                ) + '/' + resources.getResourceTypeName(resId) + '/' + resources.getResourceEntryName(
-                    resId
-                )
-            )
-
-            val testRegistry = object : ActivityResultRegistry() {
-                override fun <I, O> onLaunch(
-                    requestCode: Int,
-                    contract: ActivityResultContract<I, O>,
-                    input: I,
-                    options: ActivityOptionsCompat?
-                ) {
-                    dispatchResult(requestCode, expectedResult)
-                }
-            }
-            return testRegistry
-        }
-    }
 
     private var testUserUid: String = ""
     private val TEST_PASSWORD: String = "123456"
