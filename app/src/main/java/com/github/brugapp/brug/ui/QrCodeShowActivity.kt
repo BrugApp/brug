a54a6b431c717139a48c1f6defd261e5f8aab70b
Mounir Raki <mouniraki@protonmail.com>
2022-05-27 00:25:43 +0200

    Fix QrCodeShowActivity code display issues

--- app/src/main/java/com/github/brugapp/brug/ui/QrCodeShowActivity.kt
+++ app/src/main/java/com/github/brugapp/brug/ui/QrCodeShowActivity.kt
@@ -18,11 +18,10 @@ class QrCodeShowActivity : AppCompatActivity() {
         setContentView(R.layout.activity_qr_code_show)
         //get Extra from intent
         val qrId: String? = intent.getStringExtra("qrId")
-        val itemName: String? = intent.getStringExtra("itemName")
         //print to log the qrId
 
         val itemNameText = findViewById<TextView>(R.id.itemNameShow)
-        itemNameText.text = itemName!!
+        itemNameText.text = qrId!!
 
         val imageView = findViewById<ImageView>(R.id.showQrCode)
         //create Bitmap from string qrId
